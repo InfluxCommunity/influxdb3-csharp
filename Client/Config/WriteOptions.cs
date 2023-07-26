292de82ecad6a440ee5551b61018d3044c4265e0
alespour <42931850+alespour@users.noreply.github.com>
2023-07-26 12:42:03 +0200

    fix: add doc string
    Co-authored-by: Jakub Bednář <jakub.bednar@gmail.com>

--- Client/Config/WriteOptions.cs
+++ Client/Config/WriteOptions.cs
@@ -2,6 +2,27 @@ using InfluxDB3.Client.Write;
 
 namespace InfluxDB3.Client.Config;
 
+/// <summary>
+/// The WriteOptions class holds the configuration for writing data to InfluxDB.
+///
+/// You can configure following options:
+/// - Precision: The default precision to use for the timestamp of points if no precision is specified in the write API call.
+/// - GzipThreshold: The threshold in bytes for gzipping the body. The default value is 1000.
+///
+/// If you want create client with custom options, you can use the following code:
+/// <code>
+/// using var client = new InfluxDBClient(new InfluxDBClientConfigs{
+///     HostUrl = "https://us-east-1-1.aws.cloud2.influxdata.com",
+///     Organization = "my-org",
+///     Database = "my-database",
+///     WriteOptions = new WriteOptions
+///    {
+///        Precision = WritePrecision.S,
+///         GzipThreshold = 4096
+///    }
+/// }); 
+/// </code>
+/// </summary>
 public class WriteOptions
 {
     /// <summary>
