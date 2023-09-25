52dc98477e090b1fe4d5bbae46e93d2bd2087a04
Jakub Bednar <jakub.bednar@gmail.com>
2023-09-25 13:01:58 +0200

    feat: add downsampling skeleton

---
+++ Examples/Downsampling/DownsamplingExample.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using InfluxDB3.Client;
+
+namespace InfluxDB3.Examples.Downsampling;
+
+public class DownsamplingExample
+{
+    static async Task Main(string[] args)
+    {
+        const string host = "https://us-east-1-1.aws.cloud2.influxdata.com";
+        const string token = "my-token";
+        const string database = "my-database";
+
+        using var client = new InfluxDBClient(host: host, token: token, database: database);
+    }
+}
