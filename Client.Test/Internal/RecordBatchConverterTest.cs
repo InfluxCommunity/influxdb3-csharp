220f13cd8a26599fc1fcb93da878c48c3a2bd078
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2024-12-10 15:07:43 +0700

    fix: lint

--- Client.Test/Internal/RecordBatchConverterTest.cs
+++ Client.Test/Internal/RecordBatchConverterTest.cs
@@ -1,6 +1,8 @@
+using System;
 using Apache.Arrow;
 using Apache.Arrow.Types;
 using InfluxDB3.Client.Internal;
+using InfluxDB3.Client.Write;
 
 namespace InfluxDB3.Client.Test.Internal;
 
@@ -9,11 +11,34 @@ public class RecordBatchConverterTest
     [Test]
     public void ConvertToPointDataValue()
     {
+        Schema schema;
+        RecordBatch recordBatch;
+        PointDataValues point;
+        
         var stringField = new Field("measurement", StringType.Default, true);
         var stringArray = new StringArray.Builder().Append("host").Build();
-        var schema = new Schema(new[] { stringField, }, null);
-        var recordBatch = new RecordBatch(schema, new[] { stringArray }, 1);
-        var point = RecordBatchConverter.ConvertToPointDataValue(recordBatch, 0);
+        schema = new Schema(new[] { stringField, }, null);
+        recordBatch = new RecordBatch(schema, new[] { stringArray }, 1);
+        point = RecordBatchConverter.ConvertToPointDataValue(recordBatch, 0);
         Assert.That(point.GetMeasurement(), Is.EqualTo("host"));
+        
+        Assert.Multiple(() =>
+        {
+            var now = new DateTimeOffset();
+            
+            var timeField = new Field("time", TimestampType.Default, true);
+            var timeArray = new TimestampArray.Builder().Append(now).Build();
+            schema = new Schema(new[] { timeField, }, null);
+            recordBatch = new RecordBatch(schema, new[] { timeArray }, 1);
+            
+            point = RecordBatchConverter.ConvertToPointDataValue(recordBatch, 0);
+            Assert.That(point.GetTimestamp(), Is.EqualTo(TimestampConverter.GetNanoTime(now.UtcDateTime)));
+            
+            timeField = new Field("test", TimestampType.Default, true);
+            schema = new Schema(new[] { timeField, }, null);
+            recordBatch = new RecordBatch(schema, new[] { timeArray }, 1);
+            point = RecordBatchConverter.ConvertToPointDataValue(recordBatch, 0);
+            Assert.That(point.GetField("test"), Is.EqualTo(now));
+        });
     }
 }
