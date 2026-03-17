8cfe885bfc4ccddb0bb02772f78b439c82cf82f0
NguyenHoangSon96 <hoangsonnguyen4321@gmail.com>
2026-03-17 16:03:35 +0700

    Revert "test: add ConvertToPointDataValueWithNull test for"
    This reverts commit f2bb3184765de41d2e81be80cb6ed41b7dc88a91.

--- Client.Test/Internal/RecordBatchConverterTest.cs
+++ Client.Test/Internal/RecordBatchConverterTest.cs
@@ -41,35 +41,4 @@ public class RecordBatchConverterTest
             Assert.That(point.GetField("test"), Is.EqualTo(now));
         });
     }
-
-    [Test]
-    public void ConvertToPointDataValueWithNull()
-    {
-        Schema schema;
-        RecordBatch recordBatch;
-        PointDataValues point;
-
-        var stringField = new Field("measurement", StringType.Default, true);
-        var stringArray = new StringArray.Builder().Append("host").Append("host").Build();
-
-        var stringField1 = new Field("stringField1", StringType.Default, true);
-        var stringArray1 = new StringArray.Builder().AppendNull().Append("normal string").Build();
-        schema = new Schema(new[] { stringField, stringField1 }, null);
-        recordBatch = new RecordBatch(schema, new[] { stringArray, stringArray1 }, 2);
-
-        point = RecordBatchConverter.ConvertToPointDataValue(recordBatch, 0);
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(point.GetMeasurement(), Is.EqualTo("host"));
-            Assert.That(point.GetField("stringField1"), Is.Null);
-        }
-
-        point = RecordBatchConverter.ConvertToPointDataValue(recordBatch, 1);
-        using (Assert.EnterMultipleScope())
-        {
-            Assert.That(point.GetMeasurement(), Is.EqualTo("host"));
-            Assert.That(point.GetField("stringField1"), Is.EqualTo("normal string"));
-        }
-
-    }
 }
