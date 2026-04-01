f68316065159ef41bbfc673f7020a78eed271804
sonnh <46211823+NguyenHoangSon96@users.noreply.github.com>
2026-04-01 17:19:56 +0700

    feat: add WriteWithInterceptor example (#227)

---
+++ Examples/General/WriteWithInterceptor.cs
@@ -0,0 +1,42 @@
+using InfluxDB3.Client;
+using InfluxDB3.Client.Config;
+
+namespace InfluxDB3.Examples.General;
+
+public class WriteWithInterceptor
+{
+    static async Task Main(string[] args)
+    {
+        var headerInterceptor = new HeaderInterceptorHandler();
+        var httpClient = new HttpClient(headerInterceptor);
+        httpClient.Timeout = TimeSpan.FromSeconds(10);
+
+        using var client = new InfluxDBClient(new ClientConfig
+        {
+            Host = Environment.GetEnvironmentVariable("INFLUXDB_URL") ??
+                   "https://us-east-1-1.aws.cloud2.influxdata.com",
+            Token = Environment.GetEnvironmentVariable("INFLUXDB_TOKEN") ?? "my-token",
+            Database = Environment.GetEnvironmentVariable("INFLUXDB_DATABASE") ?? "my-database",
+            HttpClient = httpClient,
+        });
+
+        await client.WriteRecordAsync("weather,type=test value=42.0");
+        httpClient.Dispose();
+    }
+
+    public static async Task Run()
+    {
+        await Main(Array.Empty<string>());
+    }
+}
+
+internal class HeaderInterceptorHandler() : DelegatingHandler(new HttpClientHandler())
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        request.Headers.Add("X-Client-ID", "my-client-id");
+        var response = await base.SendAsync(request, cancellationToken);
+        return response;
+    }
+}
